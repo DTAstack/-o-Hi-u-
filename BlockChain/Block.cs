ecd43f17388cf67e2e1e6d5eec75cd023290ea91
DTAstack <105832641+DTAstack@users.noreply.github.com>
2020-07-28 19:38:28 +0000

    alpha: Don't export SOCK_NONBLOCK to user space.

--- BlockChain/Block.cs
+++ BlockChain/Block.cs
@@ -6,3 +6,7 @@ namespace BlockChain
 {
     public class Block
     {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Transaction[] Transactions { get; set; }
+        public int Proof { get; set; }
