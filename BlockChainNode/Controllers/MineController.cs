f232c5a328b275a94914fb51436b7b197547061a
DTAstack <105832641+DTAstack@users.noreply.github.com>
2021-05-21 20:39:40 +0000

    powerpc: Fix DSCR inheritance in copy_thread()

--- BlockChainNode/Controllers/MineController.cs
+++ BlockChainNode/Controllers/MineController.cs
@@ -4,3 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using BlockChain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
