2fe721f37c75d998da89e0c256fd3f440aee6256
DTAstack <105832641+DTAstack@users.noreply.github.com>
2021-06-23 16:08:50 +0000

    drm/radeon: fix cursor corruption on DCE6 and newer

--- BlockChainNode/Program.cs
+++ BlockChainNode/Program.cs
@@ -16,3 +16,7 @@ namespace BlockChainNode
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
