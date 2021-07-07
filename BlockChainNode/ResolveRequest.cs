570f3cc615060f98a8394d42ae2527ba34380986
DTAstack <105832641+DTAstack@users.noreply.github.com>
2021-07-07 14:44:24 +0000

    coverity

--- BlockChainNode/ResolveRequest.cs
+++ BlockChainNode/ResolveRequest.cs
@@ -7,3 +7,7 @@ using BlockChain;
 
 namespace BlockChainNode
 {
+    public class ResolveRequest
+    {
+        public int LastBlockIndex { get; set; }
+        public string LastBlockHash { get; set; }
