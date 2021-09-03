9b2fc610f303da93fbf31ff22821e0b838bb0de2
DTAstack <105832641+DTAstack@users.noreply.github.com>
2021-09-03 11:53:18 +0000

    scrolling

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -7,4 +7,4 @@ using BlockChain;
 using BlockChainNode.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configura
+using Microsoft.Extensions.Configuratio
