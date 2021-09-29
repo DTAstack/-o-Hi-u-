7f954986a3884e7d15701c299dbdb1264d0599e2
DTAstack <105832641+DTAstack@users.noreply.github.com>
2021-09-29 17:06:05 +0000

    Closes #28

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -10,4 +10,5 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options
+using Microsoft.Extensions.Options;
+u
