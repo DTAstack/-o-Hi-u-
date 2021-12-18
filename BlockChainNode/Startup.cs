1c20841f1182691f8408d3f771d701bb7ec0a98e
DTAstack <105832641+DTAstack@users.noreply.github.com>
2021-12-18 14:57:38 +0000

    spelling.

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -22,4 +22,4 @@ namespace BlockChainNode
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { g
+        public IConfiguration Configuration { get; }
