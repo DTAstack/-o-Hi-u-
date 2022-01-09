9012b81970a2baedffb1f2cd61d33ffeb9f3458a
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-01-09 17:49:19 +0000

    fix migrations

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -24,4 +24,4 @@ namespace BlockChainNode
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Us
+        // This method gets called by the runtime. Use 
