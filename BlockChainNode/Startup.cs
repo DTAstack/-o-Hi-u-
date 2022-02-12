891e2a2787098670e7bb00c7a5092c9f02d508c7
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-02-12 12:48:44 +0000

    updating readme.md

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -25,4 +25,4 @@ namespace BlockChainNode
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        publi
+        public v
