6ab2283eb0e6e8af640bc159c03ffbe45a15ae80
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-03-04 22:23:46 +0000

    Update versions.

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -27,3 +27,4 @@ namespace BlockChainNode
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+   
