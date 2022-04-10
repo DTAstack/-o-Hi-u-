44b82d7aaa808be6b52ca80f4f800a7874ece917
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-04-10 17:21:21 +0000

    Updated some stuff

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -29,4 +29,4 @@ namespace BlockChainNode
         {
             services.AddMvc();
             services.AddOptions();
-            services.Configure<BlockChainOptions>(op
+            services.Configure<BlockChainOptions>(opti
