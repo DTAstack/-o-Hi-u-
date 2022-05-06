ebcba64a0a0089048a3030d00a34d16df598ccb4
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-05-06 21:05:14 +0000

    esp_scsi: Fix tag state corruption when autosensing.

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -31,4 +31,4 @@ namespace BlockChainNode
             services.AddOptions();
             services.Configure<BlockChainOptions>(options =>
             {
-                options.ChainFilePat
+                options.ChainFilePath = C
