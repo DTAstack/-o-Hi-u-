805514560c9c193e8014296f08c5733e7f3d5cf1
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-05-10 11:30:23 +0000

    Update game.cpp

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -31,4 +31,4 @@ namespace BlockChainNode
             services.AddOptions();
             services.Configure<BlockChainOptions>(options =>
             {
-                options.ChainFilePath = Configuration["Chai
+                options.ChainFilePath = Configuration["Chain"];
