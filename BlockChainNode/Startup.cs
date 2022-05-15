da84d68209746775456796d09f7d31bc97a0f108
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-05-15 07:40:53 +0000

    More README improvements

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -32,4 +32,4 @@ namespace BlockChainNode
             services.Configure<BlockChainOptions>(options =>
             {
                 options.ChainFilePath = Configuration["Chain"];
-                options.NodesFilePath = Co
+                options.NodesFilePath = Confi
