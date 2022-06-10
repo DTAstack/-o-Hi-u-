1f35ca7c940c04361249e836d4e0059d0467f757
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-06-10 17:27:15 +0000

    Update ads.yml

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -35,4 +35,4 @@ namespace BlockChainNode
                 options.NodesFilePath = Configuration["Nodes"];
             });
             services.AddSingleton(sp => 
-                File.Exists(Co
+                File.Exists(Configur
