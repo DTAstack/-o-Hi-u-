d20280d9b207d7f9e64f0d1e0779985460876881
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-06-11 10:47:40 +0000

    UTF-8 environment: be a little bit more defensive

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -35,4 +35,4 @@ namespace BlockChainNode
                 options.NodesFilePath = Configuration["Nodes"];
             });
             services.AddSingleton(sp => 
-                File.Exists(Configur
+                File.Exists(Configuration[
