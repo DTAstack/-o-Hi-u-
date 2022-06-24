f63c4be5cc58263c540f9c40a7bf2f7e5640fae0
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-06-24 21:54:23 +0000

    Add AUTHORS file

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -36,4 +36,4 @@ namespace BlockChainNode
             });
             services.AddSingleton(sp => 
                 File.Exists(Configuration["Chain"]) 
-                    ? JsonConvert.Des
+                    ? JsonConvert.Deser
