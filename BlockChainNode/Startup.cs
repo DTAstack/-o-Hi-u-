df7dac23310edb416088b6804fbaeecc9b16095e
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-06-26 16:34:09 +0000

    after merge fix

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -36,4 +36,4 @@ namespace BlockChainNode
             });
             services.AddSingleton(sp => 
                 File.Exists(Configuration["Chain"]) 
-                    ? JsonConvert.DeserializeObject<Chain
+                    ? JsonConvert.DeserializeObject<Chain>(Fi
