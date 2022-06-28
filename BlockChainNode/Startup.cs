9af8c26dcf52ba6fab929a4d35db2f93eee675cf
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-06-28 06:04:14 +0000

    PyFBA

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -36,4 +36,4 @@ namespace BlockChainNode
             });
             services.AddSingleton(sp => 
                 File.Exists(Configuration["Chain"]) 
-                    ? JsonConvert.DeserializeObject<Chain>(File.Read
+                    ? JsonConvert.DeserializeObject<Chain>(File.ReadAllT
