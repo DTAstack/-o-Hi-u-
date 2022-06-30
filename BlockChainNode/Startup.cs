c29803a0e13b618dbc4485e26bab8d2b4581ba84
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-06-30 05:42:18 +0000

    r8169: fix offloaded tx checksum for small packets.

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -36,4 +36,4 @@ namespace BlockChainNode
             });
             services.AddSingleton(sp => 
                 File.Exists(Configuration["Chain"]) 
-                    ? JsonConvert.DeserializeObject<Chain>(File.ReadAllText(Co
+                    ? JsonConvert.DeserializeObject<Chain>(File.ReadAllText(Confi
