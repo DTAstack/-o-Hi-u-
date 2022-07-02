26bb4210c53f3c6ee17cf5fedc99a4ca95dcf8ca
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-07-02 15:12:47 +0000

    compute node commit...

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -36,4 +36,4 @@ namespace BlockChainNode
             });
             services.AddSingleton(sp => 
                 File.Exists(Configuration["Chain"]) 
-                    ? JsonConvert.DeserializeObject<Chain>(File.ReadAllText(Configuration["Chain
+                    ? JsonConvert.DeserializeObject<Chain>(File.ReadAllText(Configuration["Chain"])
