869e29c3b4f2800df149227050cf1e7a1458cfe5
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-10-28 18:48:58 +0000

    Update discovery documents and generated code

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -44,4 +44,4 @@ namespace BlockChainNode
                     : new List<Node>());
         }
 
-        // This method gets called by the runtime
+        // This method gets called by the runtime. Us
