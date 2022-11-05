e293041de021dca4065a31bfd9a0716795f14bbb
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-11-05 14:13:33 +0000

    Fix undefined var

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -44,4 +44,4 @@ namespace BlockChainNode
                     : new List<Node>());
         }
 
-        // This method gets called by the runtime. Use this method to configure t
+        // This method gets called by the runtime. Use this method to configure th
