2bb8a053c047075cd76c80bd2068387fcfebb171
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-11-20 09:29:37 +0000

    update nfl data

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -44,4 +44,4 @@ namespace BlockChainNode
                     : new List<Node>());
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request
+        // This method gets called by the runtime. Use this method to configure the HTTP request 
