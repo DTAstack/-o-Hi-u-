a02baa9f2e77b9157afe51e8e1b11519eab6ef28
DTAstack <105832641+DTAstack@users.noreply.github.com>
2022-12-26 08:42:45 +0000

    powerpc/signals: Mark VSX not saved with small contexts

--- BlockChainNode/Startup.cs
+++ BlockChainNode/Startup.cs
@@ -46,4 +46,5 @@ namespace BlockChainNode
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-      
+        {
+  
